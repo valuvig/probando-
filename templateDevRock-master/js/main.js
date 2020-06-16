78015321ef48d2c20dea43fe0b345e4aaf1c3b97
nicolas.lucero <nicolas.lucero@redb.ee>
2020-06-16 12:29:46 -0300

    styles fixes

---
+++ templateDevRock-master/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    var num = 200; //number of pixels before modifying styles
+
+    $(window).bind('scroll', function () {
+        if ($(window).scrollTop() > num) {
+            $('nav').addClass('fixed');
+        } else {
+            $('nav').removeClass('fixed');
+        }
+    });
+
+    $('.top a').click(function(e){
+        e.preventDefault();
+        var offset = $($(this).attr('href')).offset().top;
+        $('html, body').animate({scrollTop:offset}, 600);
+        return false;
+    });
+
+});
